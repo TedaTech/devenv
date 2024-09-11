aa04600715665b397959b6b79208f8c0da19a377
Freek Gruntjes <freek@gruntjes.net>
2024-09-11 11:06:01 +0200

    feat: Simplified base config and switched to kind for cluster creation

--- base.nix
+++ base.nix
@@ -1,16 +1,18 @@
 { pkgs, lib, config, inputs, ... }:
 let
-  colimaProfile = builtins.hashString "md5" config.env.DEVENV_ROOT;
+  clusterName = builtins.hashString "md5" config.env.DEVENV_ROOT;
 in
 {
   packages = [
     pkgs.git
+    pkgs.jq
     pkgs.docker
-    pkgs.colima
+    pkgs.kind
     pkgs.kubectl
     pkgs.k9s
     pkgs.kyverno-chainsaw
-    pkgs.skaffold
+    pkgs.just
+    pkgs.watchexec
     pkgs.tektoncd-cli
     pkgs.crossplane-cli
   ];
@@ -18,20 +20,4 @@ in
   languages = {
     opentofu.enable = true;
   };
-
-  processes = {
-    kubernetes.exec = "envstart";
-    skaffold.exec = "skaffold dev";
-  };
-
-  scripts.envclean.exec = "colima delete --profile dev-${colimaProfile} --force; rm -Rf ${config.env.DEVENV_ROOT}/opentofu/*.tfstate";
-  scripts.envstop.exec = "colima stop --profile dev-${colimaProfile}";
-  scripts.envstart.exec = ''colima start \
-    --profile dev-${colimaProfile} \
-    --kubernetes \
-    --cpu 8 \
-    --memory 12 \
-    --disk 10 \
-    --foreground
-  '';
 }
